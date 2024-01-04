a9ffc1634f11129ba74142f5978dfb8edddaa473
mbur1 <94140382+mbur1@users.noreply.github.com>
2024-01-04 19:20:49 +0000

    Datenbankverbindung gefixt

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine, text
 
-db_connectionstring = "mysql+pymysql://cs-306092_primaryuser:Verhuxyz667?@localhost:3306/cs-306092_scammerscanner"
+db_connectionstring = "mysql+pymysql://cs-306092_primaryuser:Verhuxyz667?@scammerscanner.net:3306/cs-306092_scammerscanner"
 
 engine = create_engine(db_connectionstring, echo=True)
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from artikel_posts"))
+
+result_dicts = []
+
+for row in result.all():
+  result_dicts.append(row)
+
+print(result_dicts)
