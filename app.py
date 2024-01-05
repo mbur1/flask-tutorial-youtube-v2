8450405fc4a648d724df73352e042136de8b621c
mbur1 <94140382+mbur1@users.noreply.github.com>
2024-01-05 14:57:08 +0000

    neue Funktionen erstmal auskommentiert

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_artikel_from_db, load_singleart_from_db
+from database import load_artikel_from_db #, load_singleart_from_db
 
 app = Flask(__name__)
 
@@ -9,12 +9,12 @@ def hello_world():
   artikel = load_artikel_from_db()
   return render_template('home.html', artikel=artikel)
 
-
+'''
 @app.route("/artikel/<id>")
 def show_artikel():
   art = load_singleart_from_db(id)
   return jsonify(art)
-
+'''
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,20 +1,22 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connectionstring = "mysql+pymysql://cs-306092_primaryuser:Verhuxyz667?@scammerscanner.net:3306/cs-306092_scammerscanner"
-#os.environ['DB_CONNECTION']
+db_connectionstring = os.environ['DB_CONNECTION']
 
 engine = create_engine(db_connectionstring, echo=True)
+
 
 
 def load_artikel_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from artikel_posts"))
+    print(engine, "Hergestellt")
     artikel = []
     for row in result.all():
       artikel.append(row)
     return artikel
 
+'''
 def load_singleart_from_db(id):
   with engine.connect() as conn:
     result = conn.execute(text("select * from artikel_posts where id = :val"), {"val": id})
@@ -23,3 +25,4 @@ def load_singleart_from_db(id):
       return None
     else:
       return dict(rows[0])
+      '''
